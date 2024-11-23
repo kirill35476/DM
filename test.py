054813bae4d3648e4a1499ec133b1cc767068375
kirill35476 <tr.kirilllll@gmail.com>
2024-11-23 15:23:19 +0300

    add

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-a = 22
-d = 23
-s = 24
-f = 25
+from tkinter import *
+
+w =
+n =
+player_size =
+x1,y1 =
+player1_color =
+x_finish =
+
+def key_handler(event):
+    pass
+
+window= Tk()
+window.title('догони меня если сможешь')
+canvas = Canvas(window,width= w,height=n,bg = 'white')
+
+player1_id =
+finish_id =
+
+window.bind('<KeyRelease>', key_handler)
+window.mainloop()
