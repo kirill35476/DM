9f51e7144ae32852dd8e46416a5c14f1b772ed04
kirill35476 <tr.kirilllll@gmail.com>
2024-11-23 15:26:45 +0300

    add

--- test.py
+++ test.py
@@ -1,4 +1,22 @@
-a = 22
-d = 23
-s = 24
-f = 25
+from tkinter import *
+
+w =800
+n =500
+player_size =100
+x1,y1 =50,50
+player1_color ='red'
+x_finish = w - 50
+
+def key_handler(event):
+    pass
+
+window= Tk()
+window.title('догони меня если сможешь')
+canvas = Canvas(window,width= w,height=n,bg = 'white')
+canvas.pack
+
+player1_id = canvas.create_rectangle(x1,y1+player_size,y1+player_size,fill = player1_color)
+finish_id = canvas.create_rectangle(x_finish,0,x_finish+10,fil = "black")
+
+window.bind('<KeyRelease>', key_handler)
+window.mainloop()
