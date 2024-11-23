a082d2b9708359a9dedeede149bef5eb17172464
kirill35476 <tr.kirilllll@gmail.com>
2024-11-23 15:31:55 +0300

    add

--- test.py
+++ test.py
@@ -8,15 +8,15 @@ player1_color ='red'
 x_finish = w - 50
 
 def key_handler(event):
-    pass
+    canvas.move(player1_id,10,0)
 
 window= Tk()
 window.title('догони меня если сможешь')
-canvas = Canvas(window,width= w,height=n,bg = 'white')
-canvas.pack
+canvas = Canvas(window,width= w,height= n,bg = 'white')
+canvas.pack()
 
-player1_id = canvas.create_rectangle(x1,y1+player_size,y1+player_size,fill = player1_color)
-finish_id = canvas.create_rectangle(x_finish,0,x_finish+10,fil = "black")
+player1_id = canvas.create_rectangle(x1,y1,x1+player_size,y1+player_size,fill = player1_color)
+finish_id = canvas.create_rectangle(x_finish, 0,x_finish+10,n,fill = "black")
 
 window.bind('<KeyRelease>', key_handler)
 window.mainloop()
